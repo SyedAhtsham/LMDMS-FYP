e201479b27089d42a5be6021f37a706d36f572ed
SyedAhtsham <syedahtshamqau@gmail.com>
2023-01-18 21:51:21 +0500

    Updated the seeder

--- database/seeders/StaffSeeder.php
+++ database/seeders/StaffSeeder.php
@@ -7,7 +7,7 @@ use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use App\Models\Staff;
-use Faker\Factory as Faker;
+//use Faker\Factory as Faker;
 
 class StaffSeeder extends Seeder
 {
@@ -18,60 +18,60 @@ class StaffSeeder extends Seeder
      */
     public function run()
     {
-        $faker = Faker::create();
+//        $faker = Faker::create();
 
         for($i=1; $i<=1; $i++) {
             $staff = new Staff;
             $staff->staffCode = "";
             $staff->name = "Ahtsham";
             $staff->email = "syedahtshamqau@gmail.com";
-            $staff->contact = $faker->phoneNumber;
-            $staff->address = $faker->address;
-            $staff->cnic = $faker->randomNumber();
-            $staff->position = 'Driver';
-            $staff->dob = $faker->date;
+            $staff->contact = "0315-5726162";
+            $staff->address = "Prince Road, North Banigala Islamabad";
+            $staff->cnic = "61101-6869830-5";
+            $staff->position = 'Manager';
+            $staff->dob = null;
             $staff->gender = 'Male';
+            $staff->staffCode = 'STM-4213';
+
+//            $staff->save();
+//            $staff1 = Staff::all();
+//            $uniqueStaffCode = 'ST';
+//            $temp = 1000;
+//            if (sizeof($staff1) > 0) {
+//                $lastIndex = $staff1[sizeof($staff1) - 1]['staff_id'];
+//            } else {
+//                $lastIndex = 0;
+//            }
+//
+//            if ($staff1[sizeof($staff1) - 1]['position'] == 'Driver') {
+//                $uniqueStaffCode .= 'D-' . $lastIndex;
+//            }elseif($staff1[sizeof($staff1) - 1]['position'] == 'Supervisor'){
+//                $uniqueStaffCode .= 'S-' . $lastIndex;
+//            }else{
+//                $uniqueStaffCode .= 'M-' . $lastIndex;
+//            }
+
+
+
+
             $staff->save();
-            $staff1 = Staff::all();
-            $uniqueStaffCode = 'ST';
-            $temp = 1000;
-            if (sizeof($staff1) > 0) {
-                $lastIndex = $staff1[sizeof($staff1) - 1]['staff_id'];
-            } else {
-                $lastIndex = 0;
-            }
-            $staffId = $lastIndex;
-            $lastIndex += $temp;
 
-            if ($staff1[sizeof($staff1) - 1]['position'] == 'Driver') {
-                $uniqueStaffCode .= 'D-' . $lastIndex;
-            }elseif($staff1[sizeof($staff1) - 1]['position'] == 'Supervisor'){
-                $uniqueStaffCode .= 'S-' . $lastIndex;
-            }else{
-                $uniqueStaffCode .= 'M-' . $lastIndex;
-            }
-
-
-            $staff->staffCode = $uniqueStaffCode;
-
-            $staff->save();
-
-            if ($staff1[sizeof($staff1) - 1]['position'] == 'Driver') {
-            $driver = new Driver;
-            $driver->staff_id = $staffId;
-            $driver->licenseNo = $faker->randomNumber(4);
-
-            $driver->yearsExp = $faker->randomNumber(1);
-            $driver->canDrive = 'Hilux, Bike, Carry';
-
-            $driver->save();
-            }
+//            if ($staff1[sizeof($staff1) - 1]['position'] == 'Driver') {
+//            $driver = new Driver;
+//            $driver->staff_id = $staffId;
+//            $driver->licenseNo = $faker->randomNumber(4);
+//
+//            $driver->yearsExp = $faker->randomNumber(1);
+//            $driver->canDrive = 'Hilux, Bike, Carry';
+//
+//            $driver->save();
+//            }
 
             $user = new User;
-            $user->email = $staff1[sizeof($staff1) - 1]['email'];
+            $user->email = "syedahtshamqau@gmail.com";
 
             $user->password = "1234";
-            $user->staff_id = $staffId;
+            $user->staff_id = $staff->staff_id;
 
             $user->save();
 
