f4c876085c6f4acfe0d1c788d338080bc95078a6
SyedAhtsham <syedahtshamqau@gmail.com>
2023-01-18 22:54:51 +0500

    Updated

--- database/seeders/StaffSeeder.php
+++ database/seeders/StaffSeeder.php
@@ -21,17 +21,17 @@ class StaffSeeder extends Seeder
 //        $faker = Faker::create();
 
         for($i=1; $i<=1; $i++) {
-//            $staff = new Staff;
-//
-//            $staff->name = "Ahtsham";
-//            $staff->email = "syedahtshamqau@gmail.com";
-//            $staff->contact = "0315-5726162";
-//            $staff->address = "Prince Road, North Banigala Islamabad";
-//            $staff->cnic = "61101-6869830-5";
-//            $staff->position = 'Manager';
-//            $staff->dob = null;
-//            $staff->gender = 'Male';
-//            $staff->staffCode = 'STM-4213';
+            $staff = new Staff;
+
+            $staff->name = "Ahtsham";
+            $staff->email = "syedahtshamqau@gmail.com";
+            $staff->contact = "0315-5726162";
+            $staff->address = "Prince Road, North Banigala Islamabad";
+            $staff->cnic = "61101-6869830-5";
+            $staff->position = 'Manager';
+            $staff->dob = null;
+            $staff->gender = 'Male';
+            $staff->staffCode = 'STM-4213';
 
 //            $staff->save();
 //            $staff1 = Staff::all();
@@ -67,25 +67,14 @@ class StaffSeeder extends Seeder
 //            $driver->save();
 //            }
 
-//            $staff->save();
-//
-//            $user = new User;
-//            $user->email = "m@gmail.com";
-//
-//            $user->password = "1234";
-//            $user->staff_id = $staff->staff_id;
+            $staff->save();
+            $user = new User;
+            $user->email = "m@gmail.com";
 
-//            $user->save();
+            $user->password = "1234";
+            $user->staff_id = $staff->staff_id;
 
-            $staff = Staff::all();
-            foreach ($staff as $st){
-                $st->delete();
-            }
-
-            $user = User::all();
-            foreach ($user as $item) {
-                $item->delete();
-            }
+            $user->save();
 
         }
 
