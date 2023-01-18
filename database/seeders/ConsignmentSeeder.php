451a42cba4a77522ee0d9e0a14d69072ae701c7e
SyedAhtsham <syedahtshamqau@gmail.com>
2023-01-18 23:28:52 +0500

    Updated

--- database/seeders/ConsignmentSeeder.php
+++ database/seeders/ConsignmentSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Area;
 use App\Models\Consignment;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
@@ -37,14 +38,33 @@ class ConsignmentSeeder extends Seeder
 //            $cons->save();
 //        }
 
+        $area1 = new Area;
+
+$area1->areaCode = "4400";
+$area1->areaName = "Barakahu/Banigala";
+$area1->extraFuel = 1.5;
+$area1->city = "Islamabad";
+
+$area1->save();
+
+        $area = new Area;
+
+        $area->areaCode = "4100";
+        $area->areaName = "G-5 Sector";
+        $area->extraFuel = 0.5;
+        $area->city = "Islamabad";
+
+        $area->save();
+
+
 
         for($i=1; $i<=90; $i++) {
             $consignment = new Consignment;
             $consignment->consCode = "";
-            $consignment->area_id = 1;
+            $consignment->area_id = $area1->area_id;
             $consignment->consWeight = 2;
 //            $consignment->consVolume = $faker->randomNumber(2);
-                        $consignment->consVolume = 25;
+                        $consignment->consVolume = 0.5;
             $consignment->toAddress = $faker->address();
             $consignment->fromAddress = $faker->address();
             $consignment->toContact = $faker->phoneNumber();
@@ -58,5 +78,27 @@ class ConsignmentSeeder extends Seeder
             $consignment->save();
 
         }
+
+        for($i=1; $i<=90; $i++) {
+            $consignment = new Consignment;
+            $consignment->consCode = "";
+            $consignment->area_id = $area->area_id;
+            $consignment->consWeight = 25;
+//            $consignment->consVolume = $faker->randomNumber(2);
+            $consignment->consVolume = 20;
+            $consignment->toAddress = $faker->address();
+            $consignment->fromAddress = $faker->address();
+            $consignment->toContact = $faker->phoneNumber();
+            $consignment->fromContact = $faker->phoneNumber();
+            $consignment->consType = "Overnight";
+            $consignment->shipper = $faker->name();
+            $consignment->consignee = $faker->name();
+//            $consignment->COD = $faker->randomNumber(4);
+            $consignment->save();
+            $consignment->consCode = "CN-".$consignment->cons_id;
+            $consignment->save();
+
+        }
+
     }
 }
