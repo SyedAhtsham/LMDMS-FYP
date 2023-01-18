284dedea8ba8daab8af2cc3d553ae30bb428a4f9
SyedAhtsham <syedahtshamqau@gmail.com>
2023-01-19 00:30:43 +0500

    Updated

--- database/seeders/ConsignmentSeeder.php
+++ database/seeders/ConsignmentSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use App\Models\Area;
 use App\Models\Consignment;
+use App\Models\VehicleType;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use Faker\Factory as Faker;
@@ -18,6 +19,38 @@ class ConsignmentSeeder extends Seeder
     public function run()
     {
 
+        $type = new VehicleType;
+        $type->volumeCap = 1200;
+        $type->weightCap = 1200;
+        $type->typeName = "Shahzore";
+        $type->save();
+        $type = new VehicleType;
+        $type->volumeCap = 600;
+        $type->weightCap = 350;
+        $type->typeName = "Carry";
+        $type->save();
+        $type = new VehicleType;
+        $type->volumeCap = 1000;
+        $type->weightCap = 450;
+        $type->typeName = "Hilux";
+        $type->save();
+        $type = new VehicleType;
+        $type->volumeCap = 30;
+        $type->weightCap = 80;
+        $type->typeName = "Bike";
+        $type->save();
+        $type = new VehicleType;
+        $type->volumeCap = 800;
+        $type->weightCap = 400;
+        $type->typeName = "Van";
+        $type->save();
+
+
+
+
+
+
+
 //        $lines = file('englishwords/addresses.txt');
 //
 //        $cons = Consignment::all();
@@ -28,7 +61,11 @@ class ConsignmentSeeder extends Seeder
 //            echo $line;
 //        }
 
-        $faker = Faker::create();
+//        $faker = Faker::create();
+
+
+
+
 
 //        $consignment = Consignment::all();
 //
@@ -56,51 +93,51 @@ class ConsignmentSeeder extends Seeder
 //
 //        $area->save();
 
-        $areas = Area::all();
+//        $areas = Area::all();
 
 
 
-        for($i=1; $i<=90; $i++) {
-            $consignment = new Consignment;
-            $consignment->consCode = "";
-            $consignment->area_id = $areas[0]->area_id;
-            $consignment->consWeight = 2;
-//            $consignment->consVolume = $faker->randomNumber(2);
-                        $consignment->consVolume = 0.5;
-            $consignment->toAddress = $faker->address();
-            $consignment->fromAddress = $faker->address();
-            $consignment->toContact = $faker->phoneNumber();
-            $consignment->fromContact = $faker->phoneNumber();
-            $consignment->consType = "Overnight";
-            $consignment->shipper = $faker->name();
-            $consignment->consignee = $faker->name();
-//            $consignment->COD = $faker->randomNumber(4);
-            $consignment->save();
-            $consignment->consCode = "CN-".$consignment->cons_id;
-            $consignment->save();
-
-        }
-
-        for($i=1; $i<=90; $i++) {
-            $consignment = new Consignment;
-            $consignment->consCode = "";
-            $consignment->area_id = $areas[1]->area_id;
-            $consignment->consWeight = 25;
-//            $consignment->consVolume = $faker->randomNumber(2);
-            $consignment->consVolume = 20;
-            $consignment->toAddress = $faker->address();
-            $consignment->fromAddress = $faker->address();
-            $consignment->toContact = $faker->phoneNumber();
-            $consignment->fromContact = $faker->phoneNumber();
-            $consignment->consType = "Overnight";
-            $consignment->shipper = $faker->name();
-            $consignment->consignee = $faker->name();
-//            $consignment->COD = $faker->randomNumber(4);
-            $consignment->save();
-            $consignment->consCode = "CN-".$consignment->cons_id;
-            $consignment->save();
-
-        }
+//        for($i=1; $i<=90; $i++) {
+//            $consignment = new Consignment;
+//            $consignment->consCode = "";
+//            $consignment->area_id = $areas[0]->area_id;
+//            $consignment->consWeight = 2;
+////            $consignment->consVolume = $faker->randomNumber(2);
+//                        $consignment->consVolume = 0.5;
+//            $consignment->toAddress = $faker->address();
+//            $consignment->fromAddress = $faker->address();
+//            $consignment->toContact = $faker->phoneNumber();
+//            $consignment->fromContact = $faker->phoneNumber();
+//            $consignment->consType = "Overnight";
+//            $consignment->shipper = $faker->name();
+//            $consignment->consignee = $faker->name();
+////            $consignment->COD = $faker->randomNumber(4);
+//            $consignment->save();
+//            $consignment->consCode = "CN-".$consignment->cons_id;
+//            $consignment->save();
+//
+//        }
+//
+//        for($i=1; $i<=90; $i++) {
+//            $consignment = new Consignment;
+//            $consignment->consCode = "";
+//            $consignment->area_id = $areas[1]->area_id;
+//            $consignment->consWeight = 25;
+////            $consignment->consVolume = $faker->randomNumber(2);
+//            $consignment->consVolume = 20;
+//            $consignment->toAddress = $faker->address();
+//            $consignment->fromAddress = $faker->address();
+//            $consignment->toContact = $faker->phoneNumber();
+//            $consignment->fromContact = $faker->phoneNumber();
+//            $consignment->consType = "Overnight";
+//            $consignment->shipper = $faker->name();
+//            $consignment->consignee = $faker->name();
+////            $consignment->COD = $faker->randomNumber(4);
+//            $consignment->save();
+//            $consignment->consCode = "CN-".$consignment->cons_id;
+//            $consignment->save();
+//
+//        }
 
     }
 }
