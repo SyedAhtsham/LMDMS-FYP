403d6d45c48b5d44d9c931cf3a999f6a6f82a3a8
SyedAhtsham <syedahtshamqau@gmail.com>
2023-01-20 18:12:15 +0500

    Updated the log in page

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -291,8 +291,8 @@
 
                     <div class="col-md-6">
                         <div class="text-center" >
-<p>For demo purpose: use admin as email and password  </p>
-                            <div class="rounded-top pt-4"  style=" margin-top:5em; height: 120px; color: white;  background-color: rgb(0, 73, 114)">
+<p>For demo purpose: use admin as email and password, since this is a management system, therefore use desktop for a good experience. Thanks.  </p>
+                            <div class="rounded-top pt-4"  style=" margin-top:3em; height: 120px; color: white;  background-color: rgb(0, 73, 114)">
                                 <h2 style="font-weight: bold">Welcome to LMDMS</h2>
                                 <h4 >Last Mile Delivery Management System</h4>
 
